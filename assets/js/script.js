2e03457d0fd8c039bb8646a4db509b23198f4939
sabrtraveler <sabrtraveler@gmail.com>
2021-12-25 18:28:13 -0500

    finished functionality of work scheduler and made the code clean

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,47 @@
-// todays date under hero/banner 
+// todays date under hero/banner
 {
-    var today = moment().format('dddd, MMMM Do YYYY: h:mm:ss a');
-    $("#currentDate").append(today);
-
+  var today = moment().format("dddd, MMMM Do YYYY: h:mm:ss a");
+  $("#currentDate").append(today);
 }
 
-//view the timeblocks for that day
-var momentTime = moment().startOf("today").add(7, "hour");
+//work day hours for the scheduler
+var workTime = moment().startOf("hour").add(14, "hour");
 var hour = moment().format("H");
 
-for (var i = 8; i < 20; i++) {
-    var timeSlot = momentTime.add(1, "hour").format("HH:mm A");
-    var currentState;
+//for loop to create the total work time slots
+for (var i = 9; i < 18; i++) {
+  var timePeriod = workTime.add(1, "hour").format("h:mm A");
+  var timeState;
 
-    // timeblock is color coded to indicate whether it is in the past, present, or future
-    if (hour == i) {
-        currentState = 'present';
-    } else if (hour > i) {
-        currentState = 'future';
-    } else if (hour < i) {
-        currentState = 'past';
-    }
+  // the time period is color graded to represent if it's in the past/present/future
+  if (hour == i) {
+    timeState = "present";
+  } else if (hour > i) {
+    timeState = "past";
+  } else if (hour < i) {
+    timeState = "future";
+  }
 
-    var newStuff =
-        `<container class="row" id='hour-${i}'>
+  // design the work day scheduler using bootstrap
+  var workScheduler = `<container class="row" id='hour-${i}'>
             <div class="col-2"></div>
-            <div class="hour w-25 p-4 col-1">${timeSlot}</div>
-            <textarea class="description w-50 p-4 col-6 ${currentState} hour-${i}"></textarea>
+            <div class="hour w-25 p-4 col-1">${timePeriod}</div>
+            <textarea class="description w-50 p-4 col-6 ${timeState} hour-${i}"></textarea>
             <button class="saveBtn w-25 p-4 col-1 fas fa-save fa-2x"></button>
             <div class="col-2">
             </div>  
         </container>
-        <p></p>`
-        ;
+        <br>`;
+  $("#schedulerContainer").append(workScheduler);
+}
 
-    $("#containAll").append(newStuff);
-
-
-
-
-};
-
+// save button function to store data in local storage
 $(".saveBtn").on("click", function () {
-    var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, value);
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, value);
 });
 
-
-for (var i = 8; i < 20; i++) {
-    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
+for (var i = 9; i < 18; i++) {
+  $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
 }
